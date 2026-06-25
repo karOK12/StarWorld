f78fd6bf91dbb716f19051468be36d0869a7d582
karOK12 <Hfsmrkarar1993@gmail.com>
2026-06-25 12:45:09 +0300

    add deposit withdrawal transfer transaction api

---
+++ api/transactions/[id].js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = async (req, res) => {
+  try {
+
+    const { id } = req.query;
+
+    const result = await pool.query(
+      "SELECT * FROM transactions WHERE user_id = $1",
+      [id]
+    );
+
+    res.status(200).json(result.rows);
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
+};
